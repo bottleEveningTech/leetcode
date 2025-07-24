688ba5ab86befda0df58828dccd22ecedd1f87db
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-07-24 23:09:48 +0530

    Remove Element Problem Solved (Beats 100%)

---
+++ 27_RemoveElement.cs
@@ -0,0 +1,35 @@
+﻿public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        if(nums.Length > 0)
+        {
+            int lastElementIndex = nums.Length - 1;
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (nums[i] != val)
+                {
+                    break;
+                }
+                lastElementIndex--;
+            }
+            for (int i = 0; i <= lastElementIndex; i++)
+            {
+                if (nums[i] == val)
+                {
+                    nums[i] = nums[lastElementIndex];
+                    do
+                    {
+                        lastElementIndex--;
+                    } while (nums[lastElementIndex] == val);
+                }
+            }
+            if(lastElementIndex < 0)
+            {
+                return 0;
+            }
+            return nums[lastElementIndex] != val ? (lastElementIndex + 1) : lastElementIndex;
+        }
+        return 0;
+    }
+}
