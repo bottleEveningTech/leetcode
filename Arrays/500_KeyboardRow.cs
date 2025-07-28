872fa388b4ad84103196d4e9e55ac7ff9a54c380
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-07-28 20:44:41 +0530

    Keyboard Row Problem Solved

---
+++ Arrays/500_KeyboardRow.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+	public string[] FindWords(string[] words)
+	{
+		char[] firstRow = "qwertyuiop".ToArray();
+		char[] secondRow = "asdfghjkl".ToArray();
+		char[] thirdRow = "zxcvbnm".ToArray();
+
+		List<string> result = new List<string>();
+		foreach (string word in words)
+		{
+				var tempWords = word.ToLower();
+				if (tempWords.All(c => firstRow.Contains(c))
+				|| tempWords.All(c => secondRow.Contains(c))
+				|| tempWords.All(c => thirdRow.Contains(c)))
+				{
+					result.Add(word);
+				}
+		}
+		return result.ToArray();
+	}
+}
