493eef4cb9e9e2d92a349e962884ac3c79bc3f34
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-07-28 19:32:48 +0530

    Island Perimeter Problem Solved

---
+++ Arrays/463_IslandPerimeter.cs
@@ -0,0 +1,38 @@
+public class Solution
+{
+    public int IslandPerimeter(int[][] grid)
+    {
+        int perimeter = 0;
+        int rows = grid.Length;
+        int columns = grid[0].Length;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+
+                if (grid[i][j] == 1)
+                {
+
+                    if (j == 0 || grid[i][j - 1] == 0)
+                    {
+                        perimeter++;
+                    }
+                    if (i == 0 || grid[i - 1][j] == 0)
+                    {
+                        perimeter++;
+                    }
+                    if (j == columns - 1 || grid[i][j + 1] == 0)
+                    {
+                        perimeter++;
+                    }
+                    if (i == rows - 1 || grid[i + 1][j] == 0)
+                    {
+                        perimeter++;
+                    }
+                }
+
+            }
+        }
+        return perimeter;
+    }
+}
