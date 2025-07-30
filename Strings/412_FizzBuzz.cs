fd03d24f4d4998caeefb9628bf934c39a40d35bf
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-07-30 21:45:04 +0530

    Added new dir for strings and Fizz Buzz Problem Solved

---
+++ Strings/412_FizzBuzz.cs
@@ -0,0 +1,29 @@
+public class Solution
+{
+    public IList<string> FizzBuzz(int n)
+    {
+        string[] arr = new string[n + 1];
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 15 == 0)
+            {
+                arr[i] = "FizzBuzz";
+            }
+            else if (i % 5 == 0)
+            {
+                arr[i] = "Buzz";
+            }
+            else if (i % 3 == 0)
+            {
+                arr[i] = "Fizz";
+            }
+            else
+            {
+                arr[i] = i.ToString();
+
+            }
+        }
+        return arr[1..];
+    }
+}
