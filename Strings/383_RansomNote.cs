41876a2a3289aeeaccfae4bb9eb8d1ad0dccdca3
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-07-30 23:42:06 +0530

    Ransom Note Problem Solved

---
+++ Strings/383_RansomNote.cs
@@ -0,0 +1,20 @@
+public class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        char[] charsToCheck = ransomNote.ToCharArray();
+        foreach (char c in magazine)
+        {
+            if (charsToCheck.Contains(c))
+            {
+                int index = Array.IndexOf(charsToCheck, c);
+                charsToCheck[index] = '1';
+            }
+        }
+        if(charsToCheck.All(c => c == '1'))
+        {
+            return true;
+        }
+        return false;
+    }
+}
