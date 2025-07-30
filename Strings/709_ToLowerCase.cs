1bf302319b881759185db66d3d37699b7c163e95
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-07-30 23:28:40 +0530

    To Lower Case Problem Solved (Beats 100%)

---
+++ Strings/709_ToLowerCase.cs
@@ -0,0 +1,20 @@
+public class Solution
+{
+    public string ToLowerCase(string s)
+    {
+        StringBuilder result = new StringBuilder();
+        foreach (char c in s)
+        {
+            if(c >= 65 && c <= 90)
+            {
+                result.Append((char)(c + 32));
+            }
+            else
+            {
+                result.Append(c);
+            }
+        }
+        return result.ToString();
+        //return s.ToLower(); Using built-in method
+    }
+}
