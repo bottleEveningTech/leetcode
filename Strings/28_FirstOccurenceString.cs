1c12e084f090ec9ca2414efb7a6312421534a3cd
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-07-31 19:54:26 +0530

    First Occurence Of String Problem Solved: Two solutions: both beats 100%

---
+++ Strings/28_FirstOccurenceString.cs
@@ -0,0 +1,44 @@
+public class Solution
+{
+    //public int StrStr(string haystack, string needle)
+    //{
+    //    for (int i = 0; i <= haystack.Length - needle.Length; i++)
+    //    {
+    //        for (int j = 0; j < needle.Length; j++)
+    //        {
+    //            if(haystack[i + j] != needle[j])
+    //            {
+    //                break;
+    //            }
+    //            if (j == needle.Length - 1) {
+    //                return i;
+    //            }
+    //        }
+
+    //    }
+    //    return -1;
+    //}
+    public int StrStr(string haystack, string needle)
+    {
+        if (haystack.Length >= needle.Length)
+        {
+            List<int> indices = new List<int>();
+
+            for (int i = 0; i <= haystack.Length - needle.Length; i++)
+            {
+                if (haystack[i] == needle[0])
+                    indices.Add(i);
+            }
+
+            for (int i = 0; i < indices.Count; i++)
+            {
+                string checkString = haystack.Substring(indices[i], needle.Length);
+                if (checkString.Equals(needle))
+                {
+                    return indices[i];
+                }
+            }
+        }
+        return -1;
+    }
+}
