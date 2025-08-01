360feaaeb2bfd631972484edd1d5967d6f7bbb30
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-01 23:18:30 +0530

    Reformat Date Problem Solved (Beats 100%)

---
+++ Strings/1507_ReformatDate.cs
@@ -0,0 +1,27 @@
+public class Solution
+{
+
+    private static readonly Dictionary<string, string> MonthMap = new()
+            {
+                {"Jan", "01"}, {"Feb", "02"}, {"Mar", "03"}, {"Apr", "04"},
+                {"May", "05"}, {"Jun", "06"}, {"Jul", "07"}, {"Aug", "08"},
+                {"Sep", "09"}, {"Oct", "10"}, {"Nov", "11"}, {"Dec", "12"}
+            };
+
+    public string ReformatDate(string date)
+    {
+
+        string[] modifiedString = date.Split(' ');
+
+        modifiedString[0] = modifiedString[0].Substring(0, modifiedString[0].Length - 2);
+        modifiedString[0] = modifiedString[0].Length == 1 ? ("0" + modifiedString[0]) : modifiedString[0];
+
+        modifiedString[1] = MonthMap[modifiedString[1]];
+        //DateTime tempDate = DateTime.ParseExact(modifiedString[1], "MMM", CultureInfo.InvariantCulture);
+        //modifiedString[1] = tempDate.ToString("MM");
+
+        return (modifiedString[2] + "-" + modifiedString[1] + "-" + modifiedString[0]);
+        //StringBuilder result = new StringBuilder();
+
+    }
+}
