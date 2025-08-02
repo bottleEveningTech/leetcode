56bb823cc7fd0618578e019b93aeb6334d9ef466
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-02 23:43:18 +0530

    Is Subsequence Problem Solved (Beats 100%)

---
+++ Strings/392_IsSubsequence.cs
@@ -0,0 +1,35 @@
+public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        var sLength = s.Length;
+        var tLength = t.Length;
+        if (sLength == tLength)
+        {
+            return string.Equals(s, t);
+        }
+        if (sLength > tLength || tLength == 0)
+        {
+            return false;
+        }
+        if (sLength == 0)
+        {
+            return true;
+        }
+        var sIndex = 0;
+        for (int i = 0; i < tLength; i++)
+        {
+            if (s[sIndex] != t[i])
+            {
+                continue;
+            }
+            sIndex++;
+            if (sIndex != sLength)
+            {
+                continue;
+            }
+            return true;
+        }
+        return false;
+    }
+}
