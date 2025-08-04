02edff48263f9f54956c03e49ab23ad1a257d61a
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-05 00:16:41 +0530

    Valid Anagram Problem Solved

---
+++ Strings/242_ValidAnagram.cs
@@ -0,0 +1,13 @@
+public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if(t.Length != s.Length)
+        {
+            return false;
+        }
+        char[] sChar = s.ToCharArray();
+        char[] tChar = t.ToCharArray();
+        return tChar.OrderBy(x => x).SequenceEqual(sChar.OrderBy(x=> x));
+    }
+}
