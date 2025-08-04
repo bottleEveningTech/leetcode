4adb7e8a4c34ed72a4f6348703cd95c7938c1d64
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-04 20:27:48 +0530

    Count & Say Problem Solved

---
+++ Strings/38_CountAndSay.cs
@@ -0,0 +1,32 @@
+public class Solution
+{
+    public string CountAndSay(int n)
+    {
+        string result = "1";
+
+        for (int i = 2; i <= n; i++)
+        {
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+
+            for (int j = 1; j < result.Length; j++)
+            {
+                if (result[j] == result[j - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    sb.Append(count);
+                    sb.Append(result[j-1]);
+                    count = 1;
+                }
+            }
+            sb.Append(count);
+            sb.Append(result[result.Length-1]);
+            result = sb.ToString();
+        }
+        return result;
+
+    }
+}
