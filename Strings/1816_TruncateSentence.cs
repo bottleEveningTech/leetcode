8a3698723f88f555d559002f2a4fc2a10b2b3748
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-08 22:30:05 +0530

    Truncate Sentence Problem Solved (Beats 100%)

---
+++ Strings/1816_TruncateSentence.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public string TruncateSentence(string s, int k)
+    {
+        string[] words = s.Split(' ');
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i< k; i++)
+        {
+            sb.Append(words[i]);
+            if(i != k - 1)
+            {
+                sb.Append(" ");
+            }
+        }
+        return sb.ToString();
+    }
+}
