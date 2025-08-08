1ec75f6b3ccd20de6afa9667187c9511b23ca834
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-08 19:40:48 +0530

    Shuffle String Problem Solved

---
+++ Strings/1528_ShuffleString.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+public partial class Solution
+{
+    public string RestoreString(string s, int[] indices)
+    {
+        StringBuilder sb = new StringBuilder(new string(' ', s.Length));
+        for (int i = 0, j = 0; i < s.Length && j < indices.Length; i++, j++) {
+            sb[indices[j]] = s[i];
+        }
+        return sb.ToString();
+    }
+}
