b2a8c04a9c84bd88c7efec470b1f90742e7a4566
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-08 13:22:45 +0530

    Height Checker Problem Solved

---
+++ Arrays/1051_HeightChecker.cs
@@ -0,0 +1,17 @@
+public class Solution
+{
+    public int HeightChecker(int[] heights)
+    {
+        int[] sortedHeights = heights.OrderBy(h => h).ToArray();
+        int wrongHeightCount = 0;
+
+        for (int i = 0; i < heights.Length; i++)
+        {
+            if (sortedHeights[i] != heights[i])
+            {
+                wrongHeightCount++;
+            }
+        }
+        return wrongHeightCount;
+    }
+}
