bfa319f70cbefbab71fdb244501c5d42db77673d
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-08 18:35:48 +0530

    Duplicate Zeroes Problem Solved (Beats 89%)

---
+++ Arrays/1089_DuplicateZeros.cs
@@ -0,0 +1,21 @@
+public class Solution
+{
+    public void DuplicateZeros(int[] arr)
+    {
+        int arrLength = arr.Length;
+        for (int i = 0; i < arrLength - 1; i++)
+        {
+            if (arr[i] == 0)
+            {
+                for (int j = arrLength - 1; j > i + 1; j--)
+                {
+                    arr[j] = arr[j - 1];
+
+                }
+                arr[i + 1] = 0;
+                i++;
+            }
+        }
+    }
+}
+
