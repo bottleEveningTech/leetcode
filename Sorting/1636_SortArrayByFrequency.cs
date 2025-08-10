b926cff4e5144bdede9e892d3fd8a1da3d8088b3
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-10 18:17:50 +0530

    Sort Array By Frequency Problem Solved Way 1 Using Built in methods

---
+++ Sorting/1636_SortArrayByFrequency.cs
@@ -0,0 +1,7 @@
+﻿public partial class Solution
+{
+    public int[] FrequencySort(int[] nums)
+    {
+        return nums.OrderBy(x => nums.Count(n=> n== x)).ThenByDescending(x => x).ToArray();
+    }
+}
