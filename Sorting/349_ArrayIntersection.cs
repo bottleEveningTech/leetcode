f7d58608135bc9eb0439a21762a20df07e65e2c8
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-10 18:00:17 +0530

    Array Intersection Problem Solved

---
+++ Sorting/349_ArrayIntersection.cs
@@ -0,0 +1,11 @@
+﻿public partial class Solution
+{
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        if(nums1.Length < nums2.Length)
+        {
+           return Intersection(nums2, nums1);
+        }
+        return nums1.Where(x => nums2.Contains(x)).Distinct().ToArray();
+    }
+}
