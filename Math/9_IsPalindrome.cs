3f1af08e352c989c1f8309103c7b8e1a9f99529d
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-11 17:55:13 +0530

    Is Palindrome Problem Solved ( Beats 99%) Added new Math dir

---
+++ Math/9_IsPalindrome.cs
@@ -0,0 +1,25 @@
+﻿
+    public partial class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if((x < 0) || (x % 10 == 0 && x != 0))
+            {
+                return false;
+            }
+
+            int half = 0;
+            int num = x;
+
+            while( num > half)
+            {
+                half = half * 10 + num % 10;
+                num = num / 10;
+            }
+
+            return (num == half) || (num == half / 10);
+        }
+    }
+
+
+
