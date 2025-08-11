3f593bcaef43f945644fbc8120a4e80c9959a43a
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-11 18:12:13 +0530

    Add digits Problem Solved (Beats 100%)

---
+++ Math/258_AddDigits.cs
@@ -0,0 +1,18 @@
+﻿public partial class Solution
+{
+    public int AddDigits(int num)
+    {
+        while (num > 9)
+        {
+            int sum = 0;
+            int x = num;
+            while (x > 0)
+            {
+                sum = sum + x % 10;
+                x = x / 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
