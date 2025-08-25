80271e1d23c6646022df5d8adfc913725bb1e526
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-26 00:34:56 +0530

    Leetcode Bank Problem Solved (Beats 100%)

---
+++ Math/1716_LeetcodeBankMoney.cs
@@ -0,0 +1,18 @@
+﻿public partial class Solution
+{
+    public int TotalMoney(int n)
+    {
+        int initialMoney = 1;
+        int totalMoney = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if(i % 7 == 0 && i != 1)
+            {
+                initialMoney++;
+            }
+            totalMoney += initialMoney + (i - 1) % 7;
+        }
+        
+        return totalMoney;
+    }
+}
