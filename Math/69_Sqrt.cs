abb87951ae0d3cfcaf101c8518497f5d2a20d98d
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-25 22:01:09 +0530

    Square root Problem Solved

---
+++ Math/69_Sqrt.cs
@@ -0,0 +1,14 @@
+﻿public partial class Solution
+{
+    public int MySqrt(int x)
+    {
+        int count = 0;
+        for(int i =1; x >= 0 ;i = i + 2)
+        {
+            x = x - i;
+            count ++;
+        }
+        return count - 1;
+
+    }
+}
