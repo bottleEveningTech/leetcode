cf2ca7b07fc89bc58ec077b35a2cb0b8e0d9a61c
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-25 23:51:11 +0530

    Excel Column Name Problem Solved

---
+++ Math/171_ExcelColumnName.cs
@@ -0,0 +1,23 @@
+﻿public partial class Solution
+{
+    public int TitleToNumber(string columnTitle)
+    {
+        int columnTitleNumber = 0;
+        //alphabetIndex * pow(26, position in column name)
+        for (int i = 0; i < columnTitle.Length; i++)
+        {
+            int power = columnTitle.Length - (i + 1);
+            int alphaValue = AlphaValue(columnTitle[i]);
+            int positionalValue = (int)(alphaValue * Math.Pow(26, power));
+            columnTitleNumber += positionalValue;
+        }
+
+        return columnTitleNumber;
+    }
+
+    static int AlphaValue(char letter)
+    {
+        return letter - 'A' + 1;
+    }
+
+}
