3af54f3a5b7e4fd92993ca5719f90c08a75c4b05
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-25 23:35:14 +0530

    Climbing Stairs Problem Solved (Beats 100%)

---
+++ Math/70_ClimbingStairs.cs
@@ -0,0 +1,18 @@
+﻿public partial class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        int a = 1;
+        int b = 2;
+
+        for(int i=3; i <= n; i++)
+        {
+            int temp = b;
+            b = a + b;
+            a = temp;
+        }
+
+        return n == 1 ? 1 : b;
+
+    }
+}
