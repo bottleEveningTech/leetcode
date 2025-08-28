232d37b1712e8254f685c23c4e78e414f6560aa5
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-28 23:42:18 +0530

    Sorted Squares Problem Solved

---
+++ Arrays/977_SquaresOfSortedArray.cs
@@ -0,0 +1,12 @@
+﻿public partial class Solution
+{
+    public int[] SortedSquares(int[] nums)
+    {
+        for(int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = nums[i] * nums[i];
+        }
+        Array.Sort(nums);
+        return nums;
+    }
+}
