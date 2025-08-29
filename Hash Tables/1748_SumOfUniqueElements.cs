73f86199c9e80b18a2e5cdc42b6a90da8c656e72
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-29 23:42:23 +0530

    Sum of Unique Elements Problem Solved (Beats 100%)

---
+++ Hash Tables/1748_SumOfUniqueElements.cs
@@ -0,0 +1,27 @@
+﻿public partial class Solution
+{
+    public int SumOfUnique(int[] nums)
+    {
+        Dictionary<int, int> freqList = new Dictionary<int, int>();
+        int sum = 0;
+        foreach (int num in nums)
+        {
+            if (!freqList.ContainsKey(num))
+            {
+                freqList[num] = 1;
+            }
+            else
+            {
+                freqList[num]++;
+            }
+        }
+        foreach (var x in freqList)
+        {
+            if (x.Value == 1)
+            {
+                sum += x.Key;
+            }
+        }
+        return sum;
+    }
+}
