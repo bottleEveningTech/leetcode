a33014b2d50d9daf9bbdb56cf9f7f42cbfae4b59
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-30 01:09:47 +0530

    Defanging IP address Problem Solved (Beats 95%)

---
+++ Strings/1108_DefangingIPAddress.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+public partial class Solution
+{
+    public string DefangIPaddr(string address)
+    {
+       StringBuilder sb = new StringBuilder();
+        foreach(char c in address)
+        {
+            if(c == '.')
+            {
+                sb.Append("[.]");
+            }
+            else
+            {
+                sb.Append(c);
+            }
+        }
+        return sb.ToString();
+    }
+}
