e19f0626ed265de7fcec3fed4fa4ffa0839ca078
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-29 23:29:16 +0530

    FInd the Difference Problem Solved

---
+++ Hash Tables/389_FindTheDifference.cs
@@ -0,0 +1,35 @@
+﻿public partial class Solution
+{
+    public char FindTheDifference(string s, string t)
+    {
+       Dictionary<char, int> freqList = new Dictionary<char, int>();
+
+        foreach(char c in s)
+        {
+            if (!freqList.ContainsKey(c))
+            {
+                freqList[c] = 0;
+            }
+            else
+            {
+                freqList[c]++;
+            }
+
+        }
+
+        foreach (char c in t)
+        {
+            if(!freqList.ContainsKey(c) || freqList[c] < 0)
+            {
+                return c;
+            }
+            else
+            {
+                freqList[c]--;
+            }
+
+        }
+
+        return ' ';
+    }
+}
