5b9fbe07c6b9cd9fcb3a89fef5dbd7cb31a67168
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-29 23:36:27 +0530

    Lucky Integer Problem Solved

---
+++ Hash Tables/1394_LuckyInteger.cs
@@ -0,0 +1,27 @@
+﻿public partial class Solution
+{
+    public int FindLucky(int[] arr)
+    {
+        Dictionary<int, int> freqList = new Dictionary<int, int>();
+        List<int> luckyNums = new List<int>();
+        foreach (int num in arr)
+        {
+            if(!freqList.ContainsKey(num))
+            {
+                freqList[num] = 1;
+            }
+            else
+            {
+                freqList[num]++;
+            }
+        }
+        foreach(var x in freqList)
+        {
+            if(x.Key == x.Value)
+            {
+                luckyNums.Add(x.Key);
+            }
+        }
+        return luckyNums.Count != 0 ? luckyNums.Max(): -1;
+    }
+}
