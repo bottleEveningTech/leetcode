52987b00cbfa0d2086832ac2043110741e073d35
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-30 00:58:14 +0530

    Find Final Value Problem Solved (Beats 100%)

---
+++ Arrays/2154_FindFinalValue.cs
@@ -0,0 +1,15 @@
+﻿public partial class Solution
+{
+    public int FindFinalValue(int[] nums, int original)
+    {
+        for(int i = 0; i< nums.Length; i++)
+        {
+            if(nums[i] == original)
+            {
+                original = original * 2;
+                i = -1;
+            }
+        }
+        return original;
+    }
+}
