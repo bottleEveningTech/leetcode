59d9ba4480f65dad2a91c218aaa760af4149fc92
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-30 00:51:14 +0530

    First Unique Element Problem Solved

---
+++ Hash Tables/387_FirstUniqueElement.cs
@@ -0,0 +1,28 @@
+﻿public partial class Solution
+{
+    public int FirstUniqChar(string s)
+    {
+        Dictionary<char, int> freqList = new Dictionary<char, int>();
+
+        foreach (char c in s)
+        {
+            if (!freqList.ContainsKey(c))
+            {
+                freqList[c] = 1;
+            }
+            else
+            {
+                freqList[c]++;
+            }
+        }
+
+        for(int i =0; i < s.Length; i++)
+        {
+            if(freqList[s[i]] == 1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
