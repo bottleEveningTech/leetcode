f962b4383057b3500d2fa2d87f9db3de98ce0a6a
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-31 17:39:08 +0530

    Consistent String Problem Solved

---
+++ Hash Tables/1684_ConsistentStrings.cs
@@ -0,0 +1,27 @@
+﻿public partial class Solution
+{
+    public int CountConsistentStrings(string allowed, string[] words)
+    {
+
+        HashSet<char> allowedChars = new HashSet<char>(allowed);
+        int count = 0;
+        foreach (var word in words)
+        {
+            HashSet<char> setWord = new HashSet<char>(word);
+            bool isConsistent = true;
+            foreach (char c in setWord)
+            {
+                if (!allowedChars.Contains(c))
+                {
+                    isConsistent = false;
+                    break;
+                }
+            }
+            if (isConsistent)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
