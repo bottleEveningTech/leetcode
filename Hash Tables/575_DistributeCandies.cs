8063c76ea0d06bcd69204ffce26f6c3f3318efaf
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-31 16:43:04 +0530

    Distributing Candies Problem Solved

---
+++ Hash Tables/575_DistributeCandies.cs
@@ -0,0 +1,9 @@
+﻿public partial class Solution
+{
+    public int DistributeCandies(int[] candyType)
+    {
+        int size = candyType.Length/2;
+        int distictTypes = new HashSet<int>(candyType).Count;
+        return (size < distictTypes) ? size : distictTypes;
+    }
+}
