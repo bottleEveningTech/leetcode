354ab3efe21719a6672217e92b40807b783019ba
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-31 17:16:49 +0530

    Good String Problem Solved

---
+++ Hash Tables/1160_GoodString.cs
@@ -0,0 +1,43 @@
+﻿public partial class Solution
+{
+    public int CountCharacters(string[] words, string chars)
+    {
+        Dictionary<char, int> charFreqCount = new Dictionary<char, int>();
+        int count = 0;
+        foreach (char c in chars)
+        {
+            if (charFreqCount.ContainsKey(c))
+            {
+                charFreqCount[c]++;
+            }
+            else
+            {
+                charFreqCount[c] = 1;
+            }
+        }
+
+        foreach (string word in words)
+        {
+            Dictionary<char, int> tempCharFreqCount = new Dictionary<char, int>(charFreqCount);
+            bool isGood = true;
+            foreach (char c in word)
+            {
+                if (!tempCharFreqCount.ContainsKey(c) || tempCharFreqCount[c] == 0)
+                {
+                    isGood = false;
+                    break;
+                }
+                else
+                {
+                    tempCharFreqCount[c]--;
+                }
+            }
+            if (isGood)
+            {
+                count += word.Length;
+            }
+
+        }
+        return count;
+    }
+}
