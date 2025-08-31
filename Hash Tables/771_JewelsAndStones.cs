d427895180cb6216b119d5c4f238d6be1c0e7d19
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-08-31 17:25:22 +0530

    Jewels and Stones Problem Solved (Beats 100%)

---
+++ Hash Tables/771_JewelsAndStones.cs
@@ -0,0 +1,45 @@
+﻿public partial class Solution
+{
+    public int NumJewelsInStones(string jewels, string stones)
+    {
+        char[] jewelArray = jewels.ToCharArray();
+        Dictionary<char, int> stoneDict = new Dictionary<char, int>();
+        int jewelCount = 0;
+
+        foreach (char stone in stones.ToCharArray())
+        {
+            if (stoneDict.ContainsKey(stone))
+            {
+                stoneDict[stone]++;
+            }
+            else
+            {
+                stoneDict[stone] = 1;
+            }
+        }
+
+        foreach (char jewel in jewelArray)
+        {
+            if(stoneDict.TryGetValue(jewel, out int count))
+            {
+               jewelCount += count;
+            }
+        }
+
+
+        //char[] jewelArray = jewels.ToCharArray();
+        //char[] stoneArray = stones.ToCharArray();
+        //int jewelCount = 0;
+        //foreach (char jewel in jewelArray)
+        //{
+        //    foreach (char stone in stoneArray)
+        //    {
+        //        if (jewel == stone)
+        //        {
+        //            jewelCount++;
+        //        }
+        //    }
+        //}
+        return jewelCount;
+    }
+}
