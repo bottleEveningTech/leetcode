dd47301b51fd6b21f2f2f8c18a3bef7448229c77
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-01 22:46:10 +0530

    Find First & Last Occurence of Element in Sorted Array using Binary Search Problem Solved (Beats 100%)

---
+++ Binary Search/34_FirstLastOccurenceSortedArray.cs
@@ -0,0 +1,46 @@
+﻿public partial class Solution
+{
+    public int[] SearchRange(int[] nums, int target)
+    {
+        int[] occurences = [-1, -1];
+
+        FindOccurence(nums, target, occurences, true);
+        FindOccurence(nums, target, occurences, false);
+
+        return occurences;
+    }
+
+    private static void FindOccurence(int[] nums, int target, int[] occurences, bool isFirstOccurence)
+    {
+        int start = 0;
+        int end = nums.Length - 1;
+
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target)
+            {
+                if (isFirstOccurence)
+                {
+                    occurences[0] = mid;
+                    end = mid - 1;
+                }
+                else
+                {
+                    occurences[1] = mid;
+                    start = mid + 1;
+                }
+
+            }
+            else if (nums[mid] < target)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+
+    }
+}
