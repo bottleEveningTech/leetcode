3b30e74daa61be7d19cee1458e126cc324cc34e3
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-02 00:44:39 +0530

    Uncommon Words From Two Strings Problem Solved (Beats 100%)

---
+++ Hash Tables/884_UncommonWords.cs
@@ -0,0 +1,27 @@
+﻿public partial class Solution
+{
+    public string[] UncommonFromSentences(string s1, string s2)
+    {
+        var words = (s1 + " " + s2).Split(' ');
+        HashSet<string> countOnce = new HashSet<string>();
+        HashSet<string> countMore = new HashSet<string>();
+       foreach(var word in words)
+        {
+            if (countOnce.Contains(word))
+            {
+                countOnce.Remove(word);
+                countMore.Add(word);
+            }
+            else if (countMore.Contains(word))
+            {
+                continue;
+            }
+            else
+            {
+                countOnce.Add(word);
+            }
+        }
+
+       return countOnce.ToArray();
+    }
+}
