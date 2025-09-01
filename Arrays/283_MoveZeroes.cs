c57a8e4831226ef52e41510e60ef989e06153756
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-01 22:58:31 +0530

    Move Zeroes Full Problem Solved

--- Arrays/283_MoveZeroes.cs
+++ Arrays/283_MoveZeroes.cs
@@ -1,19 +1,19 @@
 public partial class Solution
 {
-	public void MoveZeroes(int[] nums)
-	{
-		int swapLimit = nums.Length - 1;
-		for(int i = 0; i< swapLimit; i++)
-		{
-			for (int j = i; j < swapLimit - 1; j++)
-			{
-				if (nums[i] == 0)
-				{
-					(nums[i], nums[j]) = (nums[j], nums[i]); 
-					swapLimit--;
-				}
-			}
-			
-		}
-	}
+    public void MoveZeroes(int[] nums)
+    {
+        int swapRange = nums.Length - 1;
+        for (int i = 0; i < swapRange; i++)
+        {
+            for (int j = i + 1; j <= swapRange; j++)
+            {
+                if (nums[i] == 0 && nums[j] != 0)
+                {
+                    (nums[i], nums[j]) = (nums[j], nums[i]);
+                    break;
+                }
+            }
+
+        }
+    }
 }
