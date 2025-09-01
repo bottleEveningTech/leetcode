57fb231c43e1450b47ae24bcb4b88fb50b58ed52
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-01 22:56:14 +0530

    Happy Number Using HashSet Problem Solved

---
+++ Hash Tables/202_HappyNumber.cs
@@ -0,0 +1,30 @@
+﻿
+public partial class Solution
+{
+    public bool IsHappy(int n)
+    {
+        HashSet<int> numberWatched = new HashSet<int>();
+        while (n !=1)
+        {
+            if(numberWatched.Contains(n))
+            {
+                return false;
+            }
+            numberWatched.Add(n);
+            n = GetSum(n);
+        }
+        return true;
+    }
+
+    private int GetSum(int n)
+    {
+        int sum = 0;
+        while (n > 0)
+        {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}
