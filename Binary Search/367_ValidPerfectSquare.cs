91bcfa0573d11e5568ff27afb7484fc2e6941bc8
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-03 17:45:31 +0530

    Valid Perfect Square Problem Solved (Beats 100%)

---
+++ Binary Search/367_ValidPerfectSquare.cs
@@ -0,0 +1,32 @@
+﻿public partial class Solution
+{
+    public bool IsPerfectSquare(int num)
+    {
+        if (num == 1)
+        {
+            return true;
+        }
+        int start = 2;
+        int end = num / 2;
+
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            long squareNumber = (long)mid * mid;
+
+            if (squareNumber == num)
+            {
+                return true;
+            }
+            else if (squareNumber < num)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+}
