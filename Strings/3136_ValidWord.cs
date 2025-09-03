00706a1d7a2eff70522078071020a357930de3c0
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-04 00:32:14 +0530

    Valid Word Problem Solved (Beats 100%)

---
+++ Strings/3136_ValidWord.cs
@@ -0,0 +1,34 @@
+﻿public partial class Solution
+{
+    public bool IsValid(string word)
+    {
+        if(word.Length < 3)
+        {
+            return false;
+        }
+        var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u',
+                                          'A', 'E', 'I', 'O', 'U' };
+        bool containsVowel = false;
+        bool containsConsonant = false;
+        foreach(char c in word)
+        {
+            if (char.IsLetter(c))
+            {
+                if(vowels.Contains(c))
+                {
+                    containsVowel = true;
+                }
+                else
+                {
+                    containsConsonant = true;
+                }
+            }
+            else if (!char.IsDigit(c))
+            {
+                return false;
+            }
+        }
+
+        return containsVowel && containsConsonant;
+    }
+}
