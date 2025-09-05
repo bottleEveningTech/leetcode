08475bd7c1603578d4642dd206904d9cef7367e2
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-06 00:15:22 +0530

    Third Max Number Problem Solved (Beats 100%)

---
+++ Arrays/414_ThirdMaxNo.cs
@@ -0,0 +1,33 @@
+﻿public partial class Solution
+{
+    public int ThirdMax(int[] nums)
+    {
+        long firstMax = long.MinValue;
+        long secondMax = long.MinValue;
+        long thirdMax = long.MinValue;
+        foreach (int num in nums)
+        {
+            if (num == firstMax || num == secondMax || num == thirdMax)
+            {
+                continue;
+            }
+            if (num > firstMax)
+            {
+                thirdMax = secondMax;
+                secondMax = firstMax;
+                firstMax = num;
+            }
+            else if (num > secondMax)
+            {
+                thirdMax = secondMax;
+                secondMax = num;
+            }
+            else if (num > thirdMax)
+            {
+                thirdMax = num;
+
+            }
+        }
+        return thirdMax == long.MinValue ? (int)firstMax : (int)thirdMax;
+    }
+}
