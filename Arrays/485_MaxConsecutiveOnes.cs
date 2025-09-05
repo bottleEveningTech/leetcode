db75e41ae650ac241e2d495bbe31ad9c1ec235ee
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-06 00:32:39 +0530

    Max Consecutive Ones Problem Solved ( Beats 100%)

---
+++ Arrays/485_MaxConsecutiveOnes.cs
@@ -0,0 +1,27 @@
+﻿public partial class Solution
+{ 
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        int maxCount = 0;
+        for (int i = 0; i < nums.Length;)
+        {
+            if (nums[i] == 1)
+            {
+                int j = i + 1;
+                int count = 1;
+                while (j < nums.Length && nums[j] == 1)
+                {
+                    count++;
+                    j++;
+                }
+                i = j;
+                maxCount = (count > maxCount) ? count : maxCount;
+            }
+            else
+            {
+                    i++;
+            }
+        }
+        return maxCount;
+    }
+}
