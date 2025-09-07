ae9710d2d9bf042c7825e5d227f793bc5d19eaf5
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-07 15:00:29 +0530

    Smallest Element Greater Than Target Problem Solved (Beats 100%)

---
+++ Binary Search/744_SmallestLetterGreaterThanTarget.cs
@@ -0,0 +1,26 @@
+﻿public partial class Solution
+{
+    public char NextGreatestLetter(char[] letters, char target)
+    {
+        //similar to ceil of a number in a sorted array, but we need to return the next greater element
+
+        int start = 0;
+        int end = letters.Length - 1;
+        char res = letters[0];
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+
+            if (letters[mid] <= target)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                res = letters[mid];
+                end = mid - 1;
+            }
+        }
+        return res;
+    }
+}
