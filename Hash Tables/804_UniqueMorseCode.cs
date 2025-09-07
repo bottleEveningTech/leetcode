26636909985d66750debcda4fb02fee62aeffb01
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-07 21:33:39 +0530

    Unique Morse Code Problem Solved (Beats 92%)

---
+++ Hash Tables/804_UniqueMorseCode.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+public partial class Solution
+{
+    public int UniqueMorseRepresentations(string[] words)
+    {
+        string[] morseCodes = new string[]
+        {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        HashSet<string> transformations = new HashSet<string>();
+
+        foreach (string word in words)
+        {
+            StringBuilder morseWord = new StringBuilder();
+            foreach (char c in word)
+            {
+                morseWord.Append(morseCodes[c - 'a']);
+            }
+            transformations.Add(morseWord.ToString());
+        }
+
+        return transformations.Count;
+    }
+}
