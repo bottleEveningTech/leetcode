17bee38df54ef201924f621b8b4fcc1cc8e74b9b
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-07 15:29:58 +0530

    Sort Colours Using DNF Problem Solved (Beats 100%)

---
+++ Sorting/75_SortColours.cs
@@ -0,0 +1,33 @@
+﻿public partial class Solution
+{
+    public void SortColors(int[] nums)
+    {
+        // using DNF algorithm
+        int n = nums.Length;
+
+        int low = 0;
+        int mid = 0;
+        int high =  n - 1;
+
+        while(mid <= high)
+        {
+            if (nums[mid] == 0)
+            {
+                (nums[low], nums[mid]) = (nums[mid], nums[low]);
+                mid++;
+                low++;
+
+            }
+            else if (nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                (nums[mid], nums[high]) = (nums[high], nums[mid]);
+                high--;
+            }
+        }
+
+    }
+}
