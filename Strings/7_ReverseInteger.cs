7f6ed0752b983ff9c5195d2f08877140ea6a212f
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-07 21:42:19 +0530

    Reverse Integer Problem Solved

---
+++ Strings/7_ReverseInteger.cs
@@ -0,0 +1,19 @@
+﻿public partial class Solution
+{
+    public int Reverse(int x)
+    {
+        long rev = 0;
+
+        while(x != 0)
+        {
+            int div = x % 10;
+            x = x / 10;
+            rev = rev * 10 + div;
+            if(rev < int.MinValue || rev > int.MaxValue)
+            {
+                return 0;
+            }
+        }
+        return (int)rev;
+    }
+}
