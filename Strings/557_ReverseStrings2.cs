57a9506a1029022f5dd0596d803418814b042d59
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-11 23:07:17 +0530

    Reverse String Problem Solved (Beats 98%)

---
+++ Strings/557_ReverseStrings2.cs
@@ -0,0 +1,15 @@
+﻿public partial class Solution
+{
+    public string ReverseWords(string s)
+    {
+        string[] words = s.Split(' ');
+        for(int i =0; i < words.Length; i++)
+        {
+            char[] wordChars = words[i].ToCharArray();
+            Array.Reverse(wordChars);
+            words[i] = new string(wordChars);
+        }
+        return string.Join(" ", words);
+
+    }
+}
