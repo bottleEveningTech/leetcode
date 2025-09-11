1a28180af2581a49906afba04f17a4c685772b6d
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-11 21:10:33 +0530

    No Zero Sum Problem Solved (Beats 100%)

---
+++ Math/1317_NoZeroSum.cs
@@ -0,0 +1,28 @@
+﻿public partial class Solution
+{
+    public int[] GetNoZeroIntegers(int n)
+    {
+        for (int a = 1; a < n; a++)
+        {
+            int b = n - a;
+            if (IsNoZero(a) && IsNoZero(b))
+            {
+                return new int[] { a, b };
+            }
+        }
+        return new int[0];
+    }
+
+    private bool IsNoZero(int x)
+    {
+        while (x > 0)
+        {
+            if (x % 10 == 0)
+            {
+                return false;
+            }
+            x = x / 10;
+        }
+        return true;
+    }
+}
