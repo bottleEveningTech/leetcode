3004a4d186f947de7673f17c584a08d3de0cf7ca
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-11 22:48:40 +0530

    Intersection Of Two Arrays Problem Solved (Beats 95%)

---
+++ Hash Tables/350_IntersectionTwoArrays2.cs
@@ -0,0 +1,33 @@
+﻿public partial class Solution
+{
+    public int[] Intersect(int[] nums1, int[] nums2)
+    {
+        Dictionary<int, int> freqCount = new Dictionary<int, int>();
+        List<int> result = new List<int>();
+        if(nums1.Length > nums2.Length)
+        {
+            var temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+        }
+        foreach (int num in nums1)
+        {
+            if (!freqCount.ContainsKey(num))
+            {
+                freqCount[num] = 0;
+            }
+           freqCount[num]++;
+
+        }
+
+        foreach(int num in nums2)
+        {
+            if (freqCount.ContainsKey(num) && freqCount[num] > 0)
+            {
+                result.Add(num);
+                freqCount[num]--;
+            }
+        }
+        return result.ToArray();
+    }
+}
