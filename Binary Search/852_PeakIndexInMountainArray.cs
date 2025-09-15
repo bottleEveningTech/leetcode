80aa5b3ba0dea8cd72153e9b9155e59531cd2f3d
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-15 18:32:47 +0530

    Peak Index In Bitonic Array Problem Solved (Beats 100%)

---
+++ Binary Search/852_PeakIndexInMountainArray.cs
@@ -0,0 +1,56 @@
+﻿public partial class Solution
+{
+    public int PeakIndexInMountainArray(int[] arr)
+    {
+        //this is a bitonic array means it increases then decreases
+
+        int start = 0;
+        int end = arr.Length - 1;
+
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+
+            if (mid > 0 && mid < arr.Length - 1)
+            {
+                if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
+                {
+                    return mid;
+                }
+                else if (arr[mid - 1] < arr[mid] && arr[mid] < arr[mid + 1])
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+            else if (mid == 0)
+            {
+                if (arr[0] > arr[1])
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            else if (mid == arr.Length - 1)
+            {
+                if (arr[arr.Length - 1] > arr[arr.Length - 2])
+                {
+                    return arr.Length - 1;
+                }
+                else
+                {
+                    return arr.Length - 2;
+                }
+
+            }
+        }
+        return start;
+    }
+
+}
