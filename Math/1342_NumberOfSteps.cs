796599aa986d171bc0f21701ecaa98f9ca3af449
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-20 23:43:44 +0530

    Number Of Steps Problem Solved (Beats 100%)

---
+++ Math/1342_NumberOfSteps.cs
@@ -0,0 +1,35 @@
+﻿public partial class Solution
+{
+    public int NumberOfSteps(int num)
+    {
+        int steps = 0;
+        //Approach 1: Using normal math
+        //while (num > 0)
+        //{
+        //    if (num % 2 == 0)
+        //    {
+        //        num /= 2;
+        //    }
+        //    else
+        //    {
+        //        num -= 1;
+        //    }
+        //    steps++;
+        //}
+
+        //Approach 2: Using bit manipulation
+        while(num > 0)
+        {
+            if((num & 1) == 0)
+            {
+                num >>= 1;
+            }
+            else
+            {
+                num--;
+            }
+            steps++;
+        }
+        return steps;
+    }
+}
