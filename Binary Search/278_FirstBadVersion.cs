7d90ed1e9fdb9543d5c2a8d0d6079887834a1cdf
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-26 21:03:45 +0530

    First Bad Version Problem Solved (Beats 89%)

---
+++ Binary Search/278_FirstBadVersion.cs
@@ -0,0 +1,25 @@
+﻿/* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); */
+
+public partial class Solution : VersionControl
+{
+    public int FirstBadVersion(int n)
+    {
+        int start = 1;
+        int end = n;
+
+        while(start < end)
+        {
+            int mid = start + (end - start) / 2;
+            if (IsBadVersion(mid))
+            {
+                end = mid;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return end;
+    }
+}
