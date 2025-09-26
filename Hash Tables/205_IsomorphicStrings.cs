84a8d530ad06d922e3bfcaaac319a60d57f8f51c
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-27 01:26:37 +0530

    Isomorphic Strings Problem Solved

---
+++ Hash Tables/205_IsomorphicStrings.cs
@@ -0,0 +1,29 @@
+﻿public partial class Solution
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        if(s.Length != t.Length)
+        {
+            return false;
+        }
+        return GetPattern(s) == GetPattern(t);
+    }
+    private string GetPattern(string str)
+    {
+        var map = new Dictionary<char, int>();
+        var pattern = new List<int>();
+        int id = 0;
+
+        foreach (char c in str)
+        {
+            if (!map.ContainsKey(c))
+            {
+                map[c] = id++;
+            }
+            pattern.Add(map[c]);
+        }
+
+        return string.Join(",", pattern);
+    }
+
+}
